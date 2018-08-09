ce8eecf6c2ddc8e54994e7d5a4c1ca9ebed1dd6b
Chris Combs <chriscombs@Chriss-MacBook-Pro-2.local>
2018-08-09 16:58:29 -0400

    Added exercises, skills

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SelectField
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-
+CHARACTER_LIST = [(0, "Any"), (1, "Fox"), (2, "Falco"), (3, "Marth"), (4, "Sheik"), (5, "Jigglypuff"),
+(6, "Peach"), (7, "Ice Climbers"), (8, "Captain Falcon"), (9, "Pikachu"), (10, "Samus"), (11, "Dr. Mario"),
+(12, "Yoshi"), (13, "Luigi"), (14, "Ganondorf"), (15, "Mario"), (16, "Young Link"), (17, "Donkey Kong"),
+(18, "Link"), (19, "Mr. Game & Watch"), (20, "Roy"), (21, "Mewtwo"), (22, "Zelda"), (23, "Ness"), (24, "Pichu"), 
+(25, "Bowser"), (26, "Kirby")]
 
 app = Flask(__name__)
 
@@ -13,34 +17,134 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Meowmix1'
 app.config['MYSQL_DB'] = 'myflaskapp'
-
 #Acts like a dictionary! Gives things like tuple values
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
 #init MySQL
 mysql = MySQL(app)
 
+#Check if user is logged in
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash("Unauthorized, please log in", 'danger')
+			return redirect(url_for('login'))
+	return wrap
 
 @app.route('/')
 def index():
+	return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
 	return render_template('home.html')
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
 
-@app.route('/articles')
-def articles():
-	cur = mysql.connection.cursor()
-	requests = cur.execute("SELECT * FROM articles")
-	articles = cur.fetchall()
+#Skill Form Class
+class SkillForm(Form):
+	title = StringField('Title', [validators.Length(min = 1, max = 200)])
+	description = TextAreaField('Description', [validators.Length(min = 0)])
+	main = SelectField('Character', choices=CHARACTER_LIST, coerce=int)
+	opponent = SelectField('Opponent', choices=CHARACTER_LIST, coerce=int)
 
-	if requests > 0:
-		return render_template('articles.html', articles=articles)
-	else:
-		msg = "No articles found"
-		return render_template('articles.html', msg=msg)
+@app.route('/skills')
+@is_logged_in
+def skills():
+	cur = mysql.connection.cursor()
+	requests = cur.execute("SELECT * FROM skills")
+	skills = cur.fetchall()
 	cur.close()
+	if requests > 0:
+		return render_template('skills.html', skills=skills, CHARACTER_LIST=CHARACTER_LIST)
+	else:
+		msg = "No skills found, start by adding a skill!"
+		return render_template('skills.html', msg=msg)
+
+@app.route('/add_skill', methods=['GET', 'POST'])
+@is_logged_in
+def add_skill():
+	form = SkillForm(request.form)
+	if(request.method == 'POST' and form.validate()):
+		title = form.title.data
+		body = form.description.data
+		main = form.main.data
+		opponent = form.opponent.data
+
+		cur = mysql.connection.cursor()
+
+		requests = cur.execute("SELECT * FROM skills WHERE title = %s AND main = %s", (title, main))
+		if requests > 0:
+			cur.close()
+			flash("The skill '" + title + "'' is already in the database", 'danger')
+			return redirect(url_for('skills'))
+
+		cur.execute("INSERT INTO skills(title, description, main, opponent) VALUES(%s, %s, %s, %s)", (title, body, main, opponent))
+
+		mysql.connection.commit()
+
+		cur.close()
+
+		flash("Skill added", "success")
+		
+		return redirect(url_for('dashboard'))
+
+	return render_template('add_skill.html', form=form)
+
+#Article Form Class
+class ExerciseForm(Form):
+	title = StringField('Title', [validators.Length(min = 1, max = 200)])
+	description = TextAreaField('Description', [validators.Length(min = 0)])
+	skillid = SelectField('Skill Practiced', choices=[], coerce=int)
+	main = SelectField('Character', choices=CHARACTER_LIST, coerce=int)
+	opponent = SelectField('Opponent', choices=CHARACTER_LIST, coerce=int)
+
+@app.route('/exercises')
+@is_logged_in
+def exercises():
+	cur = mysql.connection.cursor()
+	requests = cur.execute("SELECT * FROM exercises")
+	exercises = cur.fetchall()
+	cur.close()
+	if requests > 0:
+		return render_template('exercises.html', exercises=exercises, CHARACTER_LIST=CHARACTER_LIST)
+	else:
+		msg = "No exercises found, start by adding an exercise!"
+		return render_template('exercises.html', msg=msg)
+
+@app.route('/add_exercise', methods=['GET', 'POST'])
+@is_logged_in
+def add_exercise():
+	cur = mysql.connection.cursor()
+	requests = cur.execute("SELECT id, title FROM skills")
+	skills = cur.fetchall()
+	choices = []
+	for skill in skills:
+		choices.append((skill['id'], skill['title']))
+	form = ExerciseForm(request.form)
+	form.skillid.choices = choices
+
+	if(request.method == 'POST' and form.validate()):
+		title = form.title.data
+		description = form.description.data
+		skillid = form.skillid.data
+		main = form.main.data
+		opponent = form.opponent.data
+		cur.execute("INSERT INTO exercises(title, skillid, description, main, opponent) VALUES(%s, %s, %s, %s, %s)", (title, skillid, description, main, opponent))
+		mysql.connection.commit()
+		cur.close()
+		flash("Exercise added", 'success')
+		return redirect(url_for('exercises'))
+
+	cur.close()
+
+	return render_template('add_exercise.html', form=form)
+
+	
 
 @app.route('/article/<string:id>')
 def article(id):
@@ -53,9 +157,7 @@ def article(id):
 
 #RegisterForm, wtform
 class RegisterForm(Form):
-	name = StringField('Name', [validators.Length(min = 1, max = 50)])
-	username = StringField('Username', [validators.Length(min = 4, max = 25)])
-	email = StringField('Email', [validators.Length(min = 6, max = 50)])
+	username = StringField('Username', [validators.Length(min = 3, max = 30)])
 	password = PasswordField('Password', [
 		validators.DataRequired(),
 		validators.EqualTo('confirm', message = "Passwords do not match")
@@ -66,16 +168,20 @@ class RegisterForm(Form):
 def register():
 	form = RegisterForm(request.form)
 	if request.method == 'POST' and form.validate():
-		name = form.name.data
-		email = form.email.data
 		username = form.username.data
 		password = sha256_crypt.encrypt(str(form.password.data))
 
 		#Create Cursor
 		cur = mysql.connection.cursor()
 
+		requests = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+		if requests > 0:
+			cur.close()
+			flash('Username already taken, please select another', 'danger')
+			return redirect(url_for('register'))
+
 		#Execute Query
-		cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+		cur.execute("INSERT INTO users(username, password) VALUES(%s, %s)", (username, password))
 		#commit to db
 		mysql.connection.commit()
 
@@ -98,6 +204,7 @@ def login():
 		if result > 0:
 			data = cur.fetchone()
 			password = data['password']
+			cur.close()
 			if sha256_crypt.verify(password_candidate, password):
 				app.logger.info('PASSWORD MATCHED')
 				session['logged_in'] = True
@@ -109,8 +216,9 @@ def login():
 				error = 'Invalid login'
 				app.logger.info('PASSWORD NOT MATCHED')
 				return render_template('login.html', error=error)
-			cur.close()
+			
 		else:
+			cur.close()
 			error = 'Username not found'
 			app.logger.info('NO USER')
 			return render_template('login.html', error=error)
@@ -118,16 +226,7 @@ def login():
 
 	return render_template('login.html')
 
-#Check if user is logged in
-def is_logged_in(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return f(*args, **kwargs)
-		else:
-			flash("Unauthorized, please log in", 'danger')
-			return redirect(url_for('login'))
-	return wrap
+
 
 @app.route('/logout')
 @is_logged_in
@@ -142,16 +241,17 @@ def dashboard():
 
 	cur = mysql.connection.cursor()
 
-	result = cur.execute("SELECT * FROM articles")
+	result = cur.execute("SELECT * FROM skills")
 
-	articles = cur.fetchall()
+	skills = cur.fetchall()
 
+	cur.close()
 	if result > 0:
-		return render_template('dashboard.html', articles=articles)
+		return render_template('dashboard.html', skills=skills, CHARACTER_LIST=CHARACTER_LIST)
 	else:
 		msg = 'No articles found'
 		return render_template('dashboard.html', msg=msg)
-	cur.close()
+	
 
 #Article Form Class
 class ArticleForm(Form):
